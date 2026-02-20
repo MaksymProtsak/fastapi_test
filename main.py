1b08d5ec5f28ec9184fbcee2bcb73ac092d0c918
MaksymProtsak <protsak.m@ajax.systems>
2026-02-20 11:25:02 +0200

    Bug fix. Updated README.md.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -12,6 +12,6 @@ async def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/list_items/{items}")
-def read_item(items: list, q: Optional[str] = None):
-    return {"item_id": len(items), "q": q}
+@app.get("/list_items")
+def len_item(items: List[int] = Query(), q: Optional[str] = None):
+    return {"items_len": len(items), "q": q}
