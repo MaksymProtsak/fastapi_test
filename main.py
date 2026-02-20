81bd3c0b9875e18b3398a1b5eab6b46e05d97217
MaksymProtsak <protsak.m@ajax.systems>
2026-02-20 12:08:25 +0200

    Bug fix.

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/list_items")
-def len_item(items: List[int] = Query(), q: Optional[str] = None):
+def len_item(items: List[int] = Query(...), q: Optional[str] = None):
     return {"items_len": len(items), "q": q}
